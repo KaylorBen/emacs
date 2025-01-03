6bd8977aac14bab87f8a75391cfb82c3654b41b7
Benjamin Kaylor <blkaylor22@gmail.com>
2025-01-03 03:03:26 +0000

    init

---
+++ emacs.el
@@ -0,0 +1,104 @@
+;; Disable the splash screen (to enable it agin, replace the t with 0)
+(setq inhibit-splash-screen t)
+
+;; Enable transient mark mode
+(transient-mark-mode 1)
+
+;; Performance tweaks for modern machines
+(setq gc-cons-threshold 100000000) ; 100 mb
+(setq read-process-output-max (* 1024 1024)) ; 1mb
+
+;; Remove extra UI clutter by hiding the scrollbar, menubar, and toolbar.
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; linenumbers
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
+
+;; font
+(add-to-list 'default-frame-alist '(font . "FiraCode Nerd Font Mono-42" ))
+(set-face-attribute 'default t :font "FiraCode Nerd Font Mono-42" )
+
+;; org mode
+(use-package org
+      :ensure t
+      :config
+      (require 'org)
+      (add-hook 'org-mode-hook 'org-indent-mode)
+      ;; Make Org mode work with files ending in .org
+      (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+      (use-package org-cliplink
+            :ensure t
+            :config
+            (evil-leader/set-key "m p l" 'org-cliplink))
+
+      (use-package org-modern
+            :ensure t
+            :config
+            (with-eval-after-load 'org (global-org-modern-mode))))
+
+;; evil mode
+(setq evil-want-C-u-scroll t
+      evil-want-C-u-delete t)
+(use-package evil
+      :ensure t
+      :config
+      (evil-mode 1)
+      (use-package evil-leader
+            :ensure t
+            :config
+            (global-evil-leader-mode t)
+            (evil-leader/set-leader "<SPC>"))
+            (evil-leader/set-key "q" 'kill-emacs)
+
+      (use-package evil-org
+            :ensure t
+            :config
+            (evil-org-set-key-theme
+            '(textobjects insert navigation additional shift todo heading))
+            (add-hook 'org-mode-hook (lambda () (evil-org-mode))))
+
+      (use-package powerline-evil
+            :ensure t
+            :config
+            (powerline-evil-vim-color-theme)))
+
+
+;; text-scale
+(use-package default-text-scale
+      :ensure t
+      :config
+      (default-text-scale-mode 1))
+
+;; colorscheme
+(use-package doom-themes
+      :ensure t
+      :config
+      (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+            doom-themes-enable-italic t) ; if nil, italics is universally disabled
+      (load-theme 'doom-one t)
+      (doom-themes-org-config))
+
+;; fzf
+(use-package fzf
+      :ensure t
+      :config
+      (evil-leader/set-key
+            "f d" 'fzf-directory
+            "f f" 'fzf-find-file
+            "f r" 'fzf-recentf
+            "f t" 'fzf-grep
+            "f s" 'fzf-grep-in-dir)
+      (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+            fzf/executable "fzf"
+            fzf/git-grep-args "-i --line-number %s"
+            ;; command used for `fzf-grep-*` functions
+            ;; example usage for ripgrep:
+            ;; fzf/grep-command "rg --no-heading -nH"
+            fzf/grep-command "grep -nrH"
+            ;; If nil, the fzf buffer will appear at the top of the window
+            fzf/position-bottom t
+            fzf/window-height 15))
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Flake to export Emacs package";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+  };
+
+  outputs = {self, nixpkgs, flake-utils, emacs-overlay}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [ emacs-overlay.overlay ];
+        };
+      in rec {
+        defaultPackage = packages.${system};
+
+        packages.${system} = (pkgs.emacsWithPackagesFromUsePackage {
+          config = ./emacs.el;
+          defaultInitFile = true;
+          package = pkgs.emacs-git;
+          extraEmacsPackages = epkgs: [
+            pkgs.fzf
+          ];
+        });
+      }
+    );
+}
